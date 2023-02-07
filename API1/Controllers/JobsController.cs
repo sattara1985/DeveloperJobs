1ce916e7082aeeffb24e48d057358f86a840bd2e
sattara1985 <sattara1985@gmail.com>
2023-02-07 17:37:43 -0500

    Remove default controller and add jopbsd controller

---
+++ API1/Controllers/JobsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API1.Controllers
+{
+    [ApiController]
+    [Route(template:"[controller]")]
+    public class JobsController : ControllerBase
+    {
+        
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Hola mundo Steven Api");
+        }
+    }
+}
